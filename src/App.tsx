9ec9762323e065e8b735c2800c4f0bce3dd07c84
Kishi <azusemargl@gmail.com>
2021-09-18 16:03:49 +0300

    added a main structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <form className="form">
+        <h2>Регистрация</h2>
+        <p>Уже есть аккаунт? <a href="#">Войти</a></p>
+        <div className="form__group">
+          <label htmlFor="name">Имя</label>
+          <input type="text" name="name" placeholder="Введите Ваше имя" />
+        </div>
+        <div className="form__group">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" placeholder="Введите Ваш email" />
+        </div>
+        <div className="form__group">
+          <label htmlFor="phone">Номер телефона</label>
+          <input type="text" name="phone" placeholder="Введите номер телефона" />
+        </div>
+        <div className="form__group">
+          <label htmlFor="name">Язык</label>
+          <select name="language">
+            <option value="russian">Русский</option>
+            <option value="english">Английский</option>
+            <option value="chinese">Китайский</option>
+            <option value="spanish">Испанский</option>
+          </select>
+        </div>
+        <div className="form__check">
+          <input type="checkbox" id="checkbox" />
+          <label htmlFor="checkbox">Принимаю <a href="#">условия</a> использования</label>
+        </div>
+        <button>Зарегистрироваться</button>
+      </form>
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ src/components/FormItem/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './formItem.scss'
+
+interface FormItemProps {
+   
+}
+
+export const FormItem: React.FC<FormItemProps> = () => {
+   return (
+      <div>
+         
+      </div>
+   )
+}
